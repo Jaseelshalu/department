c7c6e8bf96822bceb2962b2f78d7729fa77684c6
JaseelSha <jaseelwork098@gmail.com>
2023-07-22 22:13:24 +0530

    updated

--- config/collection.js
+++ config/collection.js
@@ -1,7 +1,4 @@
 module.exports = {
     FORM_COLLECTION: 'form',
-    USER_COLLECTION: 'user',
-    CART_COLLECTION: 'cart',
-    ORDER_COLLECTION: 'order',
-    ADMIN_COLLECTION: 'admin'
+    USER_COLLECTION: 'user'
 }
