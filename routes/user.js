aea2e21372762ac5699856f26f9a1373bee07841
JaseelSha <jaseelwork098@gmail.com>
2023-07-31 07:15:41 +0530

    31

--- routes/user.js
+++ routes/user.js
@@ -76,29 +76,29 @@ router.get('/logout', verifyLogin, (req, res) => {
   res.redirect('/')
 })
 
-router.get('/signup', (req, res) => {
-  if (req.session.user) {
-    res.redirect('/')
-  }
-  else {
-    res.render('user/signup', { loginErr: req.session.loginErr, profile: true })
-    req.session.loginErr = false
-  }
-})
+// router.get('/signup', (req, res) => {
+//   if (req.session.user) {
+//     res.redirect('/')
+//   }
+//   else {
+//     res.render('user/signup', { loginErr: req.session.loginErr, profile: true })
+//     req.session.loginErr = false
+//   }
+// })
 
-router.post('/signup', (req, res) => {
-  userHelpers.doSignup(req.body).then((response) => {
-    if (response.status) {
-      req.session.user = response.user
-      req.session.name = response.user.Name
-      req.session.user.loggedIn = true
-      res.redirect('/')
-    } else {
-      req.session.loginErr = "this email has already taken"
-      res.redirect('/signup')
-    }
-  })
-})
+// router.post('/signup', (req, res) => {
+//   userHelpers.doSignup(req.body).then((response) => {
+//     if (response.status) {
+//       req.session.user = response.user
+//       req.session.name = response.user.Name
+//       req.session.user.loggedIn = true
+//       res.redirect('/')
+//     } else {
+//       req.session.loginErr = "this email has already taken"
+//       res.redirect('/signup')
+//     }
+//   })
+// })
 
 router.get('/form', verifyLogin, async (req, res) => {
   let data = await userHelpers.unlockedItems()
